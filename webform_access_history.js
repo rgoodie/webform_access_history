b05a1c3aec1b8fb6791731fd598ac66d15c91681
rgoodie <rgoodie@users.noreply.github.com>
2016-10-17 15:17:28 -0500

    Coming together

--- webform_access_history.js
+++ webform_access_history.js
@@ -2,14 +2,27 @@
  * Created by carbon on 10/14/16.
  */
 
-
+// wait until jQ is ready.
 jQuery(function() {
 
-    // TODO: get list from Drupal object
-    console.log
 
-    // TODO: find all instances o
-    // jQuery("[href='/node/%/submission/%']")
+    // wait until the object is created
+    jQuery(Drupal.settings.webforms_access_history).ready(function() {
+
+
+        // this NID
+        var nid = Drupal.settings.webforms_access_history_guessed_nid;
+
+        // loop through, find <a> tag and add class
+        for (index in Drupal.settings.webforms_access_history) {
+            sid_number =Drupal.settings.webforms_access_history[index];
+            jQuery("[href='/node/" + nid+  "/submission/" + sid_number + "']").attr('style', 'border: 1px solid red;');
+        }
+
+
+    });
+
+
 
 })
 
